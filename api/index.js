1f9b762efc535c0acd2f90b1410397f0ff5090d3
krlspj <krls.pjds@gmail.com>
2020-10-01 01:18:24 +0200

    try to use static resources

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 
 const meals = require('./routes/meals');
 const orders = require('./routes/orders');
+const playTest = require('./routes/play');
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,6 +16,9 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 app.use('/api/meals', meals);
 app.use('/api/orders', orders);
+app.use('/api/play', playTest);
+
+app.use('/api/js', express.static('/home/krls/Documents/Repos/serverless-vercel/classInJs/js'));
 
 app.get('*/help', (req, res) => {
     res.send('hello');
